7ba9d15dbecd4f49e24dcd0e23a941adae19d95a
Christopher Hughes <christopher.hughes@fiqas.nl>
2019-04-04 16:38:44 +0200

    Initial commit.

---
+++ ApplicationPartTest/Startup.cs
@@ -0,0 +1,54 @@
+﻿using McMaster.NETCore.Plugins;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace ApplicationPartTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var mvcBuilder = services
+                .AddMvc()                
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\Module\bin\Debug\netstandard2.0\Module.dll");
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File not found: {path}");
+
+            var loader = PluginLoader.CreateFromAssemblyFile(path);
+            var pluginAssembly = loader.LoadDefaultAssembly();
+
+            var partFactory = ApplicationPartFactory.GetApplicationPartFactory(pluginAssembly);
+            foreach (var part in partFactory.GetApplicationParts(pluginAssembly))
+            {
+                Console.WriteLine($"* {part.Name}");
+                mvcBuilder.PartManager.ApplicationParts.Add(part);
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvcWithDefaultRoute();
+            app.UseMvc();
+        }
+    }
+}
---
+++ Module/ModuleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Module
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModuleController : Controller
+    {
+        public ModuleController()
+        {
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> GetFromModule()
+        {
+            return "result from module";
+        }
+
+    }
+}
